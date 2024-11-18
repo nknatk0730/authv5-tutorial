51f371a8e06ce3f346ba540e50a97d480e53b8df
nknatk0730 <nknatk0730@gmail.com>
2024-11-18 16:37:11 +0900

    fix(page): suspense register

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,7 +1,10 @@
 import RegisterForm from "@/components/auth/register-form";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <RegisterForm />
+    <Suspense fallback={<div>Loading...</div>}>
+      <RegisterForm />
+    </Suspense>
   )
 }

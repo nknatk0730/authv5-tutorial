a57b80cad2f3df144eaca7f27aa6f473defc121d
nknatk0730 <nknatk0730@gmail.com>
2024-11-18 16:33:42 +0900

    fix(page): new verification suspense

--- app/auth/new-verification/page.tsx
+++ app/auth/new-verification/page.tsx
@@ -1,7 +1,10 @@
 import { NewVerificationForm } from "@/components/auth/new-verification-form";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <NewVerificationForm />
+    <Suspense fallback={<div>Loading...</div>}>
+      <NewVerificationForm />
+    </Suspense>
   )
 }
